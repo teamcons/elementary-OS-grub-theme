0473fbd258eaf486f15a3413307a16c5a0d94791
Uncle Tallest <tallest@ohmytallest.productions>
2024-11-14 12:33:38 -0600

    adjusted startup.sh to reflect the new repo naming to prevent complaints from the like-named distro

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@
 mkdir -v /boot/grub/themes
 
 # Move theme there
-cp -vri "eOS-grub-theme" /boot/grub/themes
+cp -vri "elementary-OS-grub-theme" /boot/grub/themes
 
 # Link to it in config
-echo "GRUB_THEME=/boot/grub/themes/eOS-grub-theme/theme.txt" >> /etc/default/grub
+echo "GRUB_THEME=/boot/grub/themes/elementary-OS-grub-theme/theme.txt" >> /etc/default/grub
 
 # Update grub configuration
 update-grub
